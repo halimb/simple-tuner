6f8968d37672ffa15451de87ea5f1dc8bcf9ce12
Halim <halim.bouguedra@gmail.com>
2017-09-15 16:09:20 +0100

    implement basic microphone input using navigator.mediaDevices.getUserMedia() mediaStream and AudioContext

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 "use strict"
-console.log("yo");
+var context, source;
+var connected = false;
+
+if(navigator.mediaDevices) {
+	navigator.mediaDevices
+	.getUserMedia({audio: true, video: false})
+	.then( function(stream) {
+		context = 	new (window.AudioContext || 
+			  				 window.webkitAudioContext);
+		source = context.createMediaStreamSource(stream);
+		//source.connect(context.destination);
+		//connected = true;
+		console.log(context);
+		console.log(source);
+	})
+}
+
+var btn = document.getElementById("btn");
+btn.onclick = function() {
+	if(connected) {
+		//CONNECT
+		source.disconnect();
+		connected = false;
+		document.body.className = "off";
+	} else {
+		//DISCONNECT
+		source.connect(context.destination);
+		connected = true;
+		document.body.className = "on";
+	}
+}
+
