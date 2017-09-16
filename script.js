1f945e6fa1777736f71680ed7e1372204c2968e9
Halim <halim.bouguedra@gmail.com>
2017-09-16 16:51:34 +0100

    add visualization with frequency display

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ var context, source;
 var connected = false;
 var interval = 0;
 var c = document.getElementById("cnv");
-//c.width = 1000;
 var ctx = c.getContext("2d");
-
+var frequencies;
 if(navigator.mediaDevices) {
 	navigator.mediaDevices
 	.getUserMedia({audio: true, video: false})
@@ -27,9 +26,7 @@ display.onclick = function() {
 		source.disconnect();
 		connected = false;
 		document.body.className = "off";
-
-		window.clearInterval(interval);
-
+		animDown();
 	} else {
 		//DISCONNECT
 		//source.connect(context.destination);
@@ -41,48 +38,66 @@ display.onclick = function() {
 		source.connect(analyser);
 		analyser.smoothingTimeConstant = .85;
 		console.log(analyser);
-		var frequencies = 
+		frequencies = 
 			new Float32Array(analyser.frequencyBinCount);
 		var barWidth = c.width / frequencies.length;
-		interval = window.setInterval(
-			function(){
-				analyser.getFloatFrequencyData(frequencies);
-				
-				// var summedFreq = 
-				// 	new Float32Array(analyser.frequencyBinCount);
-				// analyser.getFloatFrequencyData(summedFreq);
-				// for(var i = 0; i < 4; i++) {
-				// 	for(j = 0; j < frequencies.length; j++) {
-				// 		analyser.getFloatFrequencyData(frequencies);
-				// 		summedFreq[i] += frequencies[i]
-				// 	}
-				// 	if(i == 3) {
-				// 		for(var j = 0; j < frequencies.length; j++) {
-				// 			summedFreq[j] /= 3;
-				// 		}
-				// 	}
-				// }
-				//console.log(summedFreq);
-				var sum = 0;
-				var index = 0;
-				c.width = c.width;
-				var max = -120;
-				for(var i  = 0; i < frequencies.length; i++) {
-					var freq = frequencies[i];
-					ctx.fillStyle = "rgb(0,0,0)";
-					ctx.fillRect(i * barWidth, 
-								100 + freq * -1, 
-								barWidth, 
-								400 - freq * -1 );
-					if(freq > max) {
-						max = freq;
-						index = i;
-					}
-					//sum += frequencies[i];
+		anim();
+	}
+
+	function anim() {
+		if(connected) {
+			analyser.getFloatFrequencyData(frequencies);
+			// var summedFreq = 
+			// 	new Float32Array(analyser.frequencyBinCount);
+			// analyser.getFloatFrequencyData(summedFreq);
+			// for(var i = 0; i < 4; i++) {
+			// 	for(j = 0; j < frequencies.length; j++) {
+			// 		analyser.getFloatFrequencyData(frequencies);
+			// 		summedFreq[i] += frequencies[i]
+			// 	}
+			// 	if(i == 3) {
+			// 		for(var j = 0; j < frequencies.length; j++) {
+			// 			summedFreq[j] /= 3;
+			// 		}
+			// 	}
+			// }
+			var sum = 0;
+			var index = 0;
+			c.width = c.width;
+			var max = -120;
+			for(var i  = 0; i < frequencies.length; i++) {
+				var freq = frequencies[i];
+				if(freq > max) {
+					max = freq;
+					index = i;
 				}
-				display.innerHTML = parseInt(22050 / 1024 * index);
-				//console.log(sum / frequencies.length);
-			},
-			50);
+				freq = Math.exp(Math.sqrt(freq * -1/3.5));
+				ctx.fillStyle = "rgb(0,0,0)";
+				ctx.fillRect(i * barWidth, 
+							freq,
+							barWidth + 1, 
+							c.height - freq ); 
+			}
+			display.innerHTML = parseInt(22050 / 1024 * index) + "Hz";
+			window.requestAnimationFrame(anim);
+		}
+	}
+	var barHeight = -1;
+	function animDown() {
+		c.width = c.width;
+		var barWidth = c.width / frequencies.length;
+		for(var i = 0; i < frequencies.length; i++) {
+			var freq = frequencies[i] + 5;
+			frequencies[i] = freq;
+			ctx.fillStyle = "rgb(0,0,0)";
+			ctx.fillRect(i * barWidth, 
+						freq * -2, 
+						barWidth + 1, 
+						freq * -2 );
+		}
+		//barHeight++;
+		if(frequencies[0] < 120) {
+			window.requestAnimationFrame(animDown);
+		}
 	}
 }
