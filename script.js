19d2c26d59605df0e6340c64c5035947a4349c62
Halim <halim.bouguedra@gmail.com>
2017-09-16 13:09:29 +0100

    first working protoyope with frequency display and decent precision

--- script.js
+++ script.js
@@ -32,30 +32,55 @@ display.onclick = function() {
 
 	} else {
 		//DISCONNECT
-		source.connect(context.destination);
+		//source.connect(context.destination);
 		connected = true;
 		document.body.className = "on";
 
 		var analyser = context.createAnalyser();
-		analyser.fftSize = 64;
+		analyser.fftSize = 2048;
 		source.connect(analyser);
+		analyser.smoothingTimeConstant = .85;
+		console.log(analyser);
 		var frequencies = 
 			new Float32Array(analyser.frequencyBinCount);
 		var barWidth = c.width / frequencies.length;
 		interval = window.setInterval(
 			function(){
-				analyser.getFloatFrequencyData(frequencies)
-				//console.log(frequencies);
+				analyser.getFloatFrequencyData(frequencies);
+				
+				// var summedFreq = 
+				// 	new Float32Array(analyser.frequencyBinCount);
+				// analyser.getFloatFrequencyData(summedFreq);
+				// for(var i = 0; i < 4; i++) {
+				// 	for(j = 0; j < frequencies.length; j++) {
+				// 		analyser.getFloatFrequencyData(frequencies);
+				// 		summedFreq[i] += frequencies[i]
+				// 	}
+				// 	if(i == 3) {
+				// 		for(var j = 0; j < frequencies.length; j++) {
+				// 			summedFreq[j] /= 3;
+				// 		}
+				// 	}
+				// }
+				//console.log(summedFreq);
 				var sum = 0;
+				var index = 0;
 				c.width = c.width;
+				var max = -120;
 				for(var i  = 0; i < frequencies.length; i++) {
+					var freq = frequencies[i];
 					ctx.fillStyle = "rgb(0,0,0)";
 					ctx.fillRect(i * barWidth, 
-								100 + frequencies[i] * -1, 
+								100 + freq * -1, 
 								barWidth, 
-								400 - frequencies[i] * -1 );
+								400 - freq * -1 );
+					if(freq > max) {
+						max = freq;
+						index = i;
+					}
 					//sum += frequencies[i];
 				}
+				display.innerHTML = parseInt(22050 / 1024 * index);
 				//console.log(sum / frequencies.length);
 			},
 			50);
